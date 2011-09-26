681ce208801e7793515e65ad7c43d6df83cc3437
krosk <krosk93@gmail.com>
2011-09-27 00:14:37 +0200

    Don't drop money if it's not killed by a player.
    This avoids abuses from people who has MobFarms, they crashed my
    economy. Most people with huge MobFarms won ~1000$ per 3 minutes at
    standard rates.

--- me/drakespirit/plugins/moneydrop/MDEntityListener.java
+++ me/drakespirit/plugins/moneydrop/MDEntityListener.java
@@ -124,98 +124,114 @@ public class MDEntityListener extends EntityListener {
 					}
 				}
 			}
-		}
-		else if(event.getEntity() instanceof CaveSpider) {
-			if(Math.random() < Settings.getCaveSpiderDropFreq()) {
-				mobDrop(event, Settings.getCaveSpiderDropMin(), Settings.getCaveSpiderDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Chicken) {
-			if(Math.random() < Settings.getChickenDropFreq()) {
-				mobDrop(event, Settings.getChickenDropMin(), Settings.getChickenDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Cow) {
-			if(Math.random() < Settings.getCowDropFreq()) {
-				mobDrop(event, Settings.getCowDropMin(), Settings.getCowDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Creeper) {
-			if(Math.random() < Settings.getCreeperDropFreq()) {
-				mobDrop(event, Settings.getCreeperDropMin(), Settings.getCreeperDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Enderman) {
-			if(Math.random() < Settings.getEndermanDropFreq()) {
-				mobDrop(event, Settings.getEndermanDropMin(), Settings.getEndermanDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Ghast) {
-			if(Math.random() < Settings.getGhastDropFreq()) {
-				mobDrop(event, Settings.getGhastDropMin(), Settings.getGhastDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Giant) {
-			if(Math.random() < Settings.getGiantDropFreq()) {
-				mobDrop(event, Settings.getGiantDropMin(), Settings.getGiantDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof HumanEntity) {
-			if(Math.random() < Settings.getHumanDropFreq()) {
-				mobDrop(event, Settings.getHumanDropMin(), Settings.getHumanDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Pig) {
-			if(Math.random() < Settings.getPigDropFreq()) {
-				mobDrop(event, Settings.getPigDropMin(), Settings.getPigDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof PigZombie) {
-			if(Math.random() < Settings.getPigzombieDropFreq()) {
-				mobDrop(event, Settings.getPigzombieDropMin(), Settings.getPigzombieDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Sheep) {
-			if(Math.random() < Settings.getSheepDropFreq()) {
-				mobDrop(event, Settings.getSheepDropMin(), Settings.getSheepDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Skeleton) {
-			if(Math.random() < Settings.getSkeletonDropFreq()) {
-				mobDrop(event, Settings.getSkeletonDropMin(), Settings.getSkeletonDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Slime) {
-			if(Math.random() < Settings.getSlimeDropFreq()) {
-				mobDrop(event, Settings.getSlimeDropMin(), Settings.getSlimeDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Spider) {
-			if(Math.random() < Settings.getSpiderDropFreq()) {
-				mobDrop(event, Settings.getSpiderDropMin(), Settings.getSpiderDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Squid) {
-			if(Math.random() < Settings.getSquidDropFreq()) {
-				mobDrop(event, Settings.getSquidDropMin(), Settings.getSquidDropMax());
-			}
-		}
-		else if(event.getEntity() instanceof Wolf) {
-			Wolf w = (Wolf) event.getEntity();
-			if(w.isTamed()) {
-				if(Math.random() < Settings.getTamedwolfDropFreq()) {
-					mobDrop(event, Settings.getTamedwolfDropMin(), Settings.getTamedwolfDropMax());
+		}else{
+			if (event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent){
+				EntityDamageByEntityEvent e = (EntityDamageByEntityEvent)event.getEntity().getLastDamageCause();
+				LivingEntity attacker;
+				if(e.getDamager() instanceof Projectile) {
+					attacker = ((Projectile) e.getDamager()).getShooter();
 				}
-			}
-			else {
-				if(Math.random() < Settings.getWildwolfDropFreq()) {
-					mobDrop(event, Settings.getWildwolfDropMin(), Settings.getWildwolfDropMax());
+				else if(e.getDamager() instanceof LivingEntity) {
+					attacker = (LivingEntity) e.getDamager();
 				}
-			}
-		}
-		else if(event.getEntity() instanceof Zombie) {
-			if(Math.random() < Settings.getZombieDropFreq()) {
-				mobDrop(event, Settings.getZombieDropMin(), Settings.getZombieDropMax());
+				else {
+					return;
+				}
+				if(attacker instanceof Player) {
+					if(event.getEntity() instanceof CaveSpider) {
+						if(Math.random() < Settings.getCaveSpiderDropFreq()) {
+							mobDrop(event, Settings.getCaveSpiderDropMin(), Settings.getCaveSpiderDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Chicken) {
+						if(Math.random() < Settings.getChickenDropFreq()) {
+							mobDrop(event, Settings.getChickenDropMin(), Settings.getChickenDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Cow) {
+						if(Math.random() < Settings.getCowDropFreq()) {
+							mobDrop(event, Settings.getCowDropMin(), Settings.getCowDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Creeper) {
+						if(Math.random() < Settings.getCreeperDropFreq()) {
+							mobDrop(event, Settings.getCreeperDropMin(), Settings.getCreeperDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Enderman) {
+						if(Math.random() < Settings.getEndermanDropFreq()) {
+							mobDrop(event, Settings.getEndermanDropMin(), Settings.getEndermanDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Ghast) {
+						if(Math.random() < Settings.getGhastDropFreq()) {
+							mobDrop(event, Settings.getGhastDropMin(), Settings.getGhastDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Giant) {
+						if(Math.random() < Settings.getGiantDropFreq()) {
+							mobDrop(event, Settings.getGiantDropMin(), Settings.getGiantDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof HumanEntity) {
+						if(Math.random() < Settings.getHumanDropFreq()) {
+							mobDrop(event, Settings.getHumanDropMin(), Settings.getHumanDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Pig) {
+						if(Math.random() < Settings.getPigDropFreq()) {
+							mobDrop(event, Settings.getPigDropMin(), Settings.getPigDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof PigZombie) {
+						if(Math.random() < Settings.getPigzombieDropFreq()) {
+							mobDrop(event, Settings.getPigzombieDropMin(), Settings.getPigzombieDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Sheep) {
+						if(Math.random() < Settings.getSheepDropFreq()) {
+							mobDrop(event, Settings.getSheepDropMin(), Settings.getSheepDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Skeleton) {
+						if(Math.random() < Settings.getSkeletonDropFreq()) {
+							mobDrop(event, Settings.getSkeletonDropMin(), Settings.getSkeletonDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Slime) {
+						if(Math.random() < Settings.getSlimeDropFreq()) {
+							mobDrop(event, Settings.getSlimeDropMin(), Settings.getSlimeDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Spider) {
+						if(Math.random() < Settings.getSpiderDropFreq()) {
+							mobDrop(event, Settings.getSpiderDropMin(), Settings.getSpiderDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Squid) {
+						if(Math.random() < Settings.getSquidDropFreq()) {
+							mobDrop(event, Settings.getSquidDropMin(), Settings.getSquidDropMax());
+						}
+					}
+					else if(event.getEntity() instanceof Wolf) {
+						Wolf w = (Wolf) event.getEntity();
+						if(w.isTamed()) {
+							if(Math.random() < Settings.getTamedwolfDropFreq()) {
+								mobDrop(event, Settings.getTamedwolfDropMin(), Settings.getTamedwolfDropMax());
+							}
+						}
+						else {
+							if(Math.random() < Settings.getWildwolfDropFreq()) {
+								mobDrop(event, Settings.getWildwolfDropMin(), Settings.getWildwolfDropMax());
+							}
+						}
+					}
+					else if(event.getEntity() instanceof Zombie) {
+						if(Math.random() < Settings.getZombieDropFreq()) {
+							mobDrop(event, Settings.getZombieDropMin(), Settings.getZombieDropMax());
+						}
+					}
+				}
 			}
 		}
 	}
